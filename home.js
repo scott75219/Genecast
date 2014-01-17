31c4ba7f9205fb889e58950aa7158867cc626744
tickTackHack <github.congos@xoxy.net>
2014-01-17 07:58:46 -0500

    added biomuta table demo

--- home.js
+++ home.js
@@ -15,6 +15,40 @@ function home() {
 	$("#select_gene_set option:first").attr('selected','selected');
 	$('#homemenu').show();
 	
+	// Initialize input buttons
+	$(".defaultText").focus(function(srcc) {
+        if ($(this).val() == $(this)[0].title) {
+            $(this).removeClass("defaultTextActive");
+            $(this).val("");
+        }
+    });
+    
+    $(".defaultText").blur(function() {
+        if ($(this).val() == "") {
+            $(this).addClass("defaultTextActive");
+            $(this).val($(this)[0].title);
+        }
+    });
+    
+    $(".defaultText").blur();
+    
+	// Page event listeners
+	$("div[data-role=page]").bind("pagebeforeshow", function (e, data) {
+    	$.mobile.silentScroll(0);
+    	$.mobile.changePage.defaults.transition = 'slide';
+	});
+	
+	// Tab event listeners
+	$("a[data-role=tab]").each(function () {
+    	var anchor = $(this);
+    	anchor.bind("click", function () {
+        	$.mobile.changePage(anchor.attr("href"), {
+            	transition: "none",
+            	changeHash: false
+        	});
+        return false;
+    	});
+	});
 	
 	// UI event listeners
 	$("#hivelogo-backbtn").click(function() {
@@ -28,6 +62,40 @@ function home() {
  		$('#hivelogo-backbtn').attr("src",img_logoback);
  		$('#homemenu').hide();
  		$('#screen-title').text('HIVE Reconnoiter');
+ 		
+ 		// BIOMUTA
+ 		
+ 		$(document).on('click', '#btn_biomuta_sbt', function(e){
+ 		
+ 			$('#debug-area').html('<p>Debug: Using preloaded demo data. User submitted: ' + $('#txt_biomuta').val().toUpperCase() + '</p>');
+ 			
+ 			// load preloaded data
+ 			for(var i = 0; i < 20; i++) { 
+	 			$('#biomuta-table tbody').append('<tr> \
+								<th scope="row"><a href="http://www.uniprot.org/uniprot/?query=accession:P04083">P04083</a></th> \
+								<td>ANXA1</a></td> \
+								<td>241</td> \
+								<td>TA</td> \
+								<td>22941189</td> \
+								<td>Lung adenocarcinoma [LUAD]</td> \
+								<td>COSMIC</td> \
+							</tr> \
+							<tr> \
+								<th scope="row"><a href="http://www.uniprot.org/uniprot/?query=accession:P04083">P04083</a></th> \
+								<td>ANXA1</td> \
+								<td>139</td> \
+								<td>ED</td> \
+								<td>22980975</td> \
+								<td>Lung adenocarcinoma [LUAD]</td> \
+								<td>COSMIC</td> \
+							</tr>');
+				}
+ 			$('#biomuta-results').show();
+ 		});
+ 		
+ 		
+ 		
+ 		// END -- BIOMUTA
  		
  		loadMetaData();
  		
@@ -82,7 +150,8 @@ function home() {
 			
  		});
  		 	
-        $('#reconnoiter-screen').show();        
+        $('#reconnoiter-screen').show();    
+        $('#navbar-reconnoiter').show();    
     });
     
     /*
