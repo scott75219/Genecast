5f5b5de80b6c0716e3d64cf47a8fe5919f4c2667
tickTackHack <github.congos@xoxy.net>
2014-02-05 13:33:10 -0500

    added func to detail page

--- home.js
+++ home.js
@@ -12,14 +12,31 @@ function truncate(string,len,showEllipsis){
       return string;
 };
 
-function polyphenNumeric(pred){
+// Takes the status description and replaces with the appropriate symbol image
+function statusConvert(status) {
+	var stat = status == 'Silver' ? '<img src="resources/images/Silver.png" height="30px" width="30px"/> ' : '<img src="resources/images/gold.png" height="30px" width="30px"/> ';
+	console.log("eclipse :: detail status: " + stat);
+	return stat;  	
+}
+
+
+// To save space convert the Polyphen-2 code to either number or color symbols
+// Possible values: benign, possibly damaging, or probably damaging
+function polyphenConvert(pred, mode){
+
+	var colorcode = 'white';
+	var numbrsymbol = '-';
+	
 	switch(pred.toLowerCase()){
-		case 'benign': return '1';
-		case 'possibly damaging': return '2';
-		case 'probably damaging': return '3';
-		case 'unknown': return 'unk';
-		default: return '-';
-	};
+		case 'benign': numbrsymbol = '1'; colorcode = 'green'; break;
+		case 'possibly damaging': numbrsymbol = '2'; colorcode = 'yellow'; break;
+		case 'probably damaging': numbrsymbol = '3'; colorcode = 'red'; break;
+		case 'unknown': numbrsymbol = 'unk'; colorcode = 'grey'; break;
+		default: return '<span style="margin-left: 3px;">-</span>';
+	}
+	
+	var colorsymbol = '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10"><rect fill="' + colorcode + '" width="100%" height="100%" stroke="blue" fill-opacity="0.5" stroke-opacity="0.8"/></svg>';
+	return mode == 'colors' ? colorsymbol : numbrsymbol;
 }
 
 function home() {
@@ -107,7 +124,7 @@ function home() {
 			// Text manipulations to fit data into table
 			var pmid = biomutaresults[i]['PMID'].split(";")[0];
 			var pmidlink = 'http://www.ncbi.nlm.nih.gov/pubmed/?term='+ biomutaresults[i]['PMID'];
-			var polyphen   = polyphenNumeric(biomutaresults[i]['Polyphen_Pred']);
+			var polyphen   = polyphenConvert(biomutaresults[i]['Polyphen_Pred'],'colors');
 			//var cancerType = truncate(biomutaresults[i]['Cancer_Type'],5,true);
 			var sourceType = truncate(biomutaresults[i]['Source'],8,true);
 			//var cancerType = biomutaresults[i]['Cancer_Type'];
@@ -116,7 +133,7 @@ function home() {
 			//var sourceType = biomutaresults[i]['Source'];
 			
 			// print out table row
-			$('#biomuta-table tbody').append('<tr  style="font-size: 10px;"> \
+			$('#biomuta-table tbody').append('<tr> \
 				<td><a href="#biomuta-detail" >' + biomutaresults[i]['Position_A'] + '</a></td> \
 				<td>' + biomutaresults[i]['Ref_A'] + '</td> \
 				<td>' + biomutaresults[i]['Var_A'] + '</td> \
@@ -184,6 +201,9 @@ function home() {
 		        	var idx = $(this).parent().children().index($(this));
 		        	//alert(idx);
 		        	$.mobile.navigate( href );
+		        	// Convert status to a symbol and description (Silver, Gold)
+		        	var statussymbol = statusConvert(biomutaresults[idx]['Status']);
+		        	
 					$('#biomuta-detail-table tbody').html(
 						'<tr><td>Gene:</td><td>'      + biomutaresults[idx]['Gene_Name'] + '</td></tr> \
 					 	<tr><td>UniProtKB:</td><td><a href="http://www.uniprot.org/uniprot/?query=accession:' + biomutaresults[idx]['UniProt_AC'] + '">' +  biomutaresults[idx]['UniProt_AC'] + '</td></tr>\
@@ -199,7 +219,7 @@ function home() {
 					 	<tr><td>PMID:</td><td>'    + biomutaresults[idx]['PMID'] + '</td></tr> \
 					 	<tr><td>Cancer Type:</td><td>'    + biomutaresults[idx]['Cancer_Type'] + '</td></tr> \
 					 	<tr><td>Source:</td><td>'    + biomutaresults[idx]['Source'] + '</td></tr> \
-					 	<tr><td>Status:</td><td>'    + biomutaresults[idx]['Status'] + '</td></tr>'				 	
+					 	<tr><td>Status:</td><td>'    + statussymbol + '</td></tr>'				 	
 					);
 					
 					// change to biomuta-detail page
@@ -226,7 +246,9 @@ function home() {
 function onLoad(){
     document.addEventListener('deviceready', function(){
 	    console.log('eclipse :: device is ready');
-		
+		// Check internet connection availability
+		if (navigator.onLine == 'offline') { alert('Error! No internet connection detected. It is required.'); return false; }
+		 
     	home();
     }, false);
 }
