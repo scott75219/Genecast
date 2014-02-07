8ef38109db4704fef4f81d2798182e307ea9863a
tickTackHack <github.congos@xoxy.net>
2014-02-06 21:47:52 -0500

    fixed centering of table headers and internet connection message

--- home.js
+++ home.js
@@ -249,8 +249,14 @@ function onLoad(){
     document.addEventListener('deviceready', function(){
 	    console.log('eclipse :: device is ready');
 		// Check internet connection availability
-		if (navigator.onLine == 'offline') { alert('Error! No internet connection detected. It is required.'); return false; }
-		 
+var networkState = navigator.connection.type;
+    if (networkState == Connection.NONE){
+                alert('No Internet');
+
+    }else{
+
+                   alert('Internet Connection there');
+                }		 
     	home();
     }, false);
 }
