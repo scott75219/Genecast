d5f7bca82619b4aa8bf0dc63da69be38d80d6ade
tickTackHack <github.congos@xoxy.net>
2014-02-13 11:45:54 -0500

    fixing menu options

--- home.js
+++ home.js
@@ -288,28 +288,28 @@ function onLoad(){
 	    console.log('eclipse :: device is ready');
 
 		
-	    var onSettings = function() {
-	        console.log("settings");
-	        alert("settings");
+	    var onAbout = function() {
+	        console.log("about");
+	        alert("About");
 	    };
 	
-	    var onHelp = function() {
-	        console.log("help");
-	        alert("help");
+	    var onUpdate = function() {
+	        console.log("update");
+	        alert("Update");
 	    };
 	
 	    var optionsmenu = new OptionsMenu({
 	        id: "optionsmenu",
 	        items: [ 
 	            [ {
-	                label: "Settings",
-	                image: "resources/images/drawable-hdpi/settings.png",
-	                action: onSettings
+	                label: "About",
+	                image: "resources/images/drawable-hdpi/ic_dialog_info.png",
+	                action: onAbout
 	            }, 
 	            {
-	                label: "Help",
-	                image: "resources/images/drawable-hdpi/help.png",
-	                action: onHelp
+	                label: "Update",
+	                image: "resources/images/drawable-hdpi/stat_sys_upload_anim0.png",
+	                action: onUpdate
 	            } ]
 	        ]
 	    });
