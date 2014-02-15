8794776c0ad2314b99fffce233f48c20e40534e7
jamal-desktop <jamal@tickTackHack.home>
2014-02-15 14:24:00 -0500

    added chart analytics

--- home.js
+++ home.js
@@ -141,28 +141,116 @@ function home() {
 	// BIOMUTA
 	
 	// generate biomuta graph
-	function generateBiomutaGraph() {
+	function generateBiomutaGraph(charttype) {
 		// categorize frequencies per cancer type
 		var frequencies = [];
 		var freq = {};
+		
+		// organize data into javascript object
 		$.each(biomutaresults, function(indx, obj){
 				var cancertype = obj['Cancer_Type'].match(/\[[A-Za-z0-9]+\]/)[0].replace('[', '').replace(']', '');
-				console.log(obj['Gene_Name'] + " " + cancertype);
-				if(!(cancertype in freq)) {freq[cancertype] = 0; }
+				//console.log(obj['Gene_Name'] + " " + cancertype);
+				if(!(cancertype in freq)) {freq[cancertype] = 1; }
 				else {freq[cancertype]++; }
 		});
-		
-		// sort
-	 /*  	frequencies.push(freq.sort(function(a, b) {
-	        return (parseInt(a[cancertype],10) > parseInt(b[cancertype],10)) ? 1 : ((parseInt(a[cancertype],10) < parseInt(b[cancertype],10)) ? -1 : 0);
-	    }));			
-		*/	  	
+  	
 		for (var key in freq) {
 	 		if (freq.hasOwnProperty(key)) {
 	    		console.log(key + " -> " + freq[key]);
+	    		frequencies.push({'Cancer Type': key, 'Frequency': freq[key]});
 	  		}
 		}
+		//console.log('eclipse :: frequencies length is ' + frequencies.length);
 		
+
+		if(charttype == 1) {
+			/*****
+			 *  
+			 * Below d3js bar chart code template from http://d3-generator.com
+			 *
+			* *****/
+			// clear chart area and add header
+			$('#biomuta-chart').html('<h3 style="text-align: center;">Frequency of Cancer Types</h3>');
+			// render chart
+			var data = frequencies;
+			var valueLabelWidth = 40; // space reserved for value labels (right)
+			var barHeight = 15; // height of one bar
+			var barLabelWidth = 50; // space reserved for bar labels
+			var barLabelPadding = 5; // padding between bar and bar labels (left)
+			var gridLabelHeight = 18; // space reserved for gridline labels
+			var gridChartOffset = 3; // space between start of grid and first bar
+			var maxBarWidth = $(window).width()- (barLabelWidth*2); // width of the bar with the max value
+			// accessor functions 
+			var barLabel = function(d) { return d['Cancer Type']; };
+			var barValue = function(d) { return parseFloat(d['Frequency']); };
+			 
+			// sorting
+			var sortedData = data.sort(function(a, b) {
+			 return d3.descending(barValue(a), barValue(b));
+			}); 
+			
+			// scales
+			var yScale = d3.scale.ordinal().domain(d3.range(0, sortedData.length)).rangeBands([0, sortedData.length * barHeight]);
+			var y = function(d, i) { return yScale(i); };
+			var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
+			var x = d3.scale.linear().domain([0, d3.max(sortedData, barValue)]).range([0, maxBarWidth]);
+			// svg container element
+			var chart = d3.select('#biomuta-chart').append("svg")
+			  .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
+			  .attr('height', gridLabelHeight + gridChartOffset + sortedData.length * barHeight);
+			// grid line labels
+			var gridContainer = chart.append('g')
+			  .attr('transform', 'translate(' + barLabelWidth + ',' + gridLabelHeight + ')'); 
+			gridContainer.selectAll("text").data(x.ticks(5)).enter().append("text")
+			  .attr("x", x)
+			  .attr("dy", -3)
+			  .attr("font-size", ".75em")
+			  .attr("text-anchor", "middle")
+			  .text(String);
+			// vertical grid lines
+			gridContainer.selectAll("line").data(x.ticks(5)).enter().append("line")
+			  .attr("x1", x)
+			  .attr("x2", x)
+			  .attr("y1", 0)
+			  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+			  .style("stroke", "#ccc");
+			// bar labels
+			var labelsContainer = chart.append('g')
+			  .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+			labelsContainer.selectAll('text').data(sortedData).enter().append('text')
+			  .attr('y', yText)
+			  .attr('stroke', 'none')
+			  .attr('fill', 'black')
+			  .attr("font-size", ".75em")
+			  .attr("dy", ".35em") // vertical-align: middle
+			  .attr('text-anchor', 'end')
+			  .text(barLabel);
+			// bars
+			var barsContainer = chart.append('g')
+			  .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+			barsContainer.selectAll("rect").data(sortedData).enter().append("rect")
+			  .attr('y', y)
+			  .attr('height', yScale.rangeBand())
+			  .attr('width', function(d) { return x(barValue(d)); })
+			  .attr('stroke', 'white')
+			  .attr('fill', 'steelblue');
+			// bar value labels
+			barsContainer.selectAll("text").data(sortedData).enter().append("text")
+			  .attr("x", function(d) { return x(barValue(d)); })
+			  .attr("y", yText)
+			  .attr("dx", 3) // padding-left
+			  .attr("dy", ".35em") // vertical-align: middle		  
+			  .attr("font-size", ".75em")
+			  .attr("text-anchor", "start") // text-align: right
+			  .attr("fill", "black")
+			  .attr("stroke", "none")
+			  .text(function(d) { return d3.round(barValue(d), 2); });
+			// start line
+			barsContainer.append("line")
+			  .attr("y1", -gridChartOffset)
+			  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+			  .style("stroke", "#000");
+		}
 	}
 	
 	// generate biomuta table
@@ -296,8 +384,9 @@ function home() {
 				populateBiomutaTable();  
 				$("#biomuta-table").show();
 				
-				// Construct graph
-				generateBiomutaGraph();
+				// Construct graph. 
+				// 1 = cancer type frequency bar graph 
+				generateBiomutaGraph(1);
 			};
 			
 			$('#biomuta-results').show();
