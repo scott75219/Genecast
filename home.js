9a8ef076ba5cfd724afe2ef462c8d9a8b54d3c00
scott75219 <scott.goldweber@gmail.com>
2014-08-25 12:36:42 -0400

    Update home.js

--- home.js
+++ home.js
@@ -308,13 +308,14 @@ function biomuta() {
 			
 		//	var cancertype = obj['Cancer_Type'].match(/\[[A-Za-z0-9]+\]/)[0].replace('[', '').replace(']', '');
 		//	if (cancertype ==''|| cancertype==null ||cancertype='undefined'){
-				var c = obj['Cancer_Type'][0];
-				var cancertype = c.substring(1, 6);
+			var c = obj['Cancer_Type'].match(/\/([\s\S]*)$/)[0];
+			if(cancertype == 'OTHERS' && obj['Cancer_Type'].match(/^[Cc]ancer(.*)/) == null) {
+				cancertype = truncate(obj['Cancer_Type'].substr(0,obj['Cancer_Type'].indexOf(' ')),8,false);
+			}
+		//	 cancertype = cancertype.substring(1, 6);
 
 		//	}
-		//	if(cancertype == 'OTHERS' && obj['Cancer_Type'].match(/^[Cc]ancer(.*)/) == null) {
-		//		cancertype = truncate(obj['Cancer_Type'].substr(0,obj['Cancer_Type'].indexOf(' ')),8,false);
-		//	}
+		
 			//console.log(obj['Gene_Name'] + " " + obj['Cancer_Type']);
 			if(!(cancertype in freq)) {freq[cancertype] = 1; }
 			else {freq[cancertype]++; }
